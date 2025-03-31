c313849eab7335214f4b2e8196490c960f07d89b
alphacrash <alphacrash@live.com>
2025-04-01 00:02:45 +0530

    Updated UI

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,40 +1,14 @@
+import HeroSubscription from "@/components/home/HeroSubscription";
 import ProfileRow from "@/components/home/ProfileRow";
-import SubscriptionItem from "@/components/home/SubscriptionItem";
-import { FlatList, Text, View } from "react-native";
+import RecentSubcriptions from "@/components/home/RecentSubcriptions";
 import { SafeAreaView } from "react-native-safe-area-context";
-
-const DATA = [
-  {
-    id: "netflix",
-    title: "Netflix",
-  },
-  {
-    id: "youtubePremium",
-    title: "YouTube Premium",
-  },
-  {
-    id: "amazonPrime",
-    title: "Amazon Prime",
-  },
-];
 
 export default function Home() {
   return (
     <SafeAreaView>
       <ProfileRow />
-      <View className="px-6 py-2">
-        <View>
-          <Text className="text-lg font-light">Recent</Text>
-          <Text className="text-xl font-bold">Subscriptions</Text>
-        </View>
-        <FlatList
-          data={DATA}
-          renderItem={({ item }) => (
-            <SubscriptionItem id={item.id} title={item.title} />
-          )}
-          keyExtractor={(item) => item.id}
-        />
-      </View>
+      <HeroSubscription />
+      <RecentSubcriptions />
     </SafeAreaView>
   );
 }
